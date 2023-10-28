80ee5052b77348464ad1eb7458a40e5a828cbcd5
alisadaintanvir <alisadaintanvir@gmail.com>
2023-10-29 02:16:49 +0600

    added random route

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import random
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 ##Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+db.init_app(app)
+
+migrate = Migrate(app, db)
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
     map_url = db.Column(db.String(500), nullable=False)
     img_url = db.Column(db.String(500), nullable=False)
     location = db.Column(db.String(250), nullable=False)
-    seats = db.Column(db.String(250), nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
     has_toilet = db.Column(db.Boolean, nullable=False)
     has_wifi = db.Column(db.Boolean, nullable=False)
-    has_sockets = db.Column(db.Boolean, nullable=False)
     can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
@@ -32,7 +36,7 @@ def home():
 
 @app.route("/random")
 def get_random_cafe():
-    cafes = db.session.query(Cafe).all()
+    cafes = Cafe.query.all()
     random_cafe = random.choice(cafes)
     return jsonify(cafe={
         "id": random_cafe.id,
